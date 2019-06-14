c55feaa8100d922829e565442f23f01f9afb1442
rakhi dawande <rakhi1997dawande@gmail.com>
2019-06-14 08:03:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function theBeatlesPlay(musicians, instruments){
 var musicianString;
 var musicianInstrument=[];
 for(var i=0; i<musicians.length; i++){
-musicianString= musicians[i] + ‘ plays ‘ + instruments[i];
-musicianInstrument[i]=musicianString;
+musicianString= musicians[i] + "" plays ‘ + instruments[i]
+musicianInstrument[i]=musicianString
 }
 return musicianInstrument;
 }
