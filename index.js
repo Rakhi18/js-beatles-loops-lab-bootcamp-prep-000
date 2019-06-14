557d98b49d8d8fa21d4e652421cde83f4f6a935b
rakhi dawande <rakhi1997dawande@gmail.com>
2019-06-14 08:09:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments){
 var musicianString;
 var musicianInstrument=[];
 for(var i=0; i<musicians.length; i++){
-musicianString= musicians[i] + "" plays ‘ + instruments[i]
+musicianString= musicians[i] + " plays"  + instruments[i]
 musicianInstrument[i]=musicianString
 }
 return musicianInstrument;
@@ -12,16 +12,16 @@ return musicianInstrument;
  
 
 const facts=[
-“He was the last Beatle to learn to drive”,
-“He was never a vegetarian”,
-“He was a choir boy and boy scout”,
-“He hated the sound of his own voice”
-];
+"He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+"He was a choir boy and boy scout",
+"He hated the sound of his own voice"
+]
 function johnLennonFacts(factsArray){
 var i=0;
 var newArray=[];
 while (factsArray.length > i){
-newArray.push(`${factsArray[i]}` + “!!!”);
+newArray.push(`${factsArray[i]}` + "!!!");
 i++;
 }
 return newArray;
